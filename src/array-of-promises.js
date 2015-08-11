d048a101a19c8f2faec979a3654fd63e11d43b5e
Hugh FD Jackson <hughfdjackson@googlemail.com>
2015-08-11 21:01:30 +0100

    Adding missing semicolons and .tern-project ignore

--- src/array-of-promises.js
+++ src/array-of-promises.js
@@ -7,15 +7,15 @@ var settlePromise = function(promise){
   return promise
     .then(report.fulfilledSettleReport)
     .catch(report.rejectedSettleReport)
-}
+};
 
 var resolveAndSettle = function(promise){
   return settlePromise(Promise.resolve(promise));
-}
+};
 
 var allSettled = function(promises){
   return Promise.all(promises.map(resolveAndSettle));
-}
+};
 
 
 // fulfilled
@@ -23,22 +23,22 @@ var filterFulfilledAndGetValues = function(settledReports){
   return settledReports
     .filter(report.isFulfilled)
     .map(report.getValue);
-}
+};
 
 var fulfilled = function(promises){
   return allSettled(promises).then(filterFulfilledAndGetValues);
-}
+};
 
 // rejected
 var filterRejectedAndGetReasons = function(settledReports){
   return settledReports
     .filter(report.isRejected)
     .map(report.getReason);
-}
+};
 
 var rejected = function(promises){
   return allSettled(promises).then(filterRejectedAndGetReasons);
-}
+};
 
 module.exports = {
   allSettled: allSettled,
